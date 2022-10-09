d858430d7c6f8593cb2107c11e29fcb7da1671f5
patience1000 <paulpatience577@gmail.com>
2022-10-09 20:39:08 +0100

    Updated responsiveness

--- Script.js
+++ Script.js
@@ -1,9 +1,9 @@
 function myFunction() {
     var x = document.getElementById("myTopnav");
-    if (x.className === "Nav-bar") {
+    if (x.className === "topnav") {
       x.className += " responsive";
     } else {
-      x.className = "Nav-bar";
+      x.className = "topnav";
     }
    }
 
